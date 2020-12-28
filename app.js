e263f1070612fa0780c5c9f53b0b352d15a16e25
aniledev <elinamcgill@gmail.com>
2020-12-28 16:38:48 -0500

    feat: adding more simple GET routes with Express

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
   res.send("hello express!");
 });
 
+// adding another route to Express app
+app.get("/burgers", (req, res) => {
+  res.send("we have juice cheese burgers!");
+});
+
+app.get("/pizza/pepperoni", (req, res) => {
+  res.send("your pizza is on the way");
+});
+
+app.get("/pizza/pineapple", (req, res) => {
+  res.send("we dont serve that here. never call again!");
+});
+
 app.listen(8000, () => {
   console.log("express server is listening on port 8000");
 });
