adcd51582e6f6af802288e0c3bbe172832a7dba6
aniledev <elinamcgill@gmail.com>
2020-12-28 16:26:44 -0500

    feat: create a simple get route in Express

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("works!");
+// require the express module in order to access the dependency
+const express = require("express");
+
+// app.get(PATH, HANDLER);
+// the HANDLER is the function to be executed
+/* To create a GET route we use the .get() method of the Express application object. */
+
+app.get("/burgers", (req, res) => {
+  res.send("We have juicy cheese burgers!");
+});
