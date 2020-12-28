f6a153f07e3e9457cf84fa612e3932bfa3d5a6df
aniledev <elinamcgill@gmail.com>
2020-12-28 16:32:40 -0500

    feat: update GET request with Express and listen to to port

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // require the express module in order to access the dependency
 const express = require("express");
+/* The express module exports a top-level function. 
+That function creates a new application object that encapsulates the functionality of your Express server. 
+Invoke the function to create the application. */
+const app = express();
 
-// app.get(PATH, HANDLER);
-// the HANDLER is the function to be executed
-/* To create a GET route we use the .get() method of the Express application object. */
+// a simple GET request using Express
+app.get("/", (req, res) => {
+  res.send("hello express!");
+});
 
-app.get("/burgers", (req, res) => {
-  res.send("We have juicy cheese burgers!");
+app.listen(8000, () => {
+  console.log("express server is listening on port 8000");
 });
